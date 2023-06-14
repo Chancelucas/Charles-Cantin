e2d989073c17313b2f26436f76c632fce470150b
Chancelucas <105455769+Chancelucas@users.noreply.github.com>
2023-06-14 12:55:45 +0200

    Création du style de la navbarre version mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import burgerMenu from '../assets/image/MenuBurger.svg'
 
 const Navigation = () => {
-  return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    <div className="navigation">
-      <ul>
-        <NavLink to={"/"}>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div className={`navigation ${isMenuOpen ? 'open' : ''}`}>
+      <div className="logo-container">
+        <NavLink to="/" onClick={closeMenu}>
+          <img className="logo" src="./Logo.png" alt="Logo Chales Cantin" />
+        </NavLink>
+      </div>
+      <button className={`menu-button ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <img src={burgerMenu} alt='Image burger' className="reorder-three-outline"/>
+      </button>
+      <ul className="menu">
+        <NavLink to="/" onClick={closeMenu}>
           <li>Accueil</li>
         </NavLink>
-        <NavLink to={"/Galerie"}>
+        <NavLink to="/Galerie" onClick={closeMenu}>
           <li>Galerie</li>
         </NavLink>
-        <NavLink>
-          <img src="./Logo.png" alt="Logo Chales Cantin" />
-        </NavLink>
-        <NavLink to={"/Tarifs"}>
+        <NavLink to="/Tarifs" onClick={closeMenu}>
           <li>Tarifs</li>
         </NavLink>
-        <NavLink to={"/Contact"}>
+        <NavLink to="/Contact" onClick={closeMenu}>
           <li>Contact</li>
         </NavLink>
       </ul>
     </div>
-
   );
 };
 
 export default Navigation;
+
+
+
