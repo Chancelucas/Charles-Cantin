392f10e6ae06245ae9e87ad3ba5840f5662bc23f
Chancelucas <105455769+Chancelucas@users.noreply.github.com>
2023-06-17 21:00:20 +0200

    respondsive de la page accueil totalement fini

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='container-footer'>
-     <ContactForm/>
-     <div className="liens-footer">
-      
-      <Link className='lien mention' to="/Mentions">Mentions Légales</Link>
-      <Link className='lien politique' to="/Politique">Politique de confidentialité</Link>
-      <Link className='lien creation' to="https://github.com/Chancelucas" target='_blank'>Site web conçu par Lucas CHANCEL</Link>
-      <p className='copyright'>Copyright © 2023 Charles Cantin - Tous droits réservés</p>
+      <ContactForm />
+      <div className="liens-footer">
 
-     </div>
+        <Link className='lien mention' to="/Mentions">Mentions Légales</Link>
+        <Link className='lien politique' to="/Politique">Politique de confidentialité</Link>
+        <a className='lien creation' href="https://github.com/Chancelucas" target='_blank' rel='noopener noreferrer'>Site web conçu par Lucas CHANCEL</a>
+        <p className='copyright'>
+          &copy; {new Date().getFullYear()} Charles Cantin - Tous droits réservés
+        </p>
+
+      </div>
     </div>
   );
 };
--- src/components/Tarifs/Tarifs.js
+++ src/components/Tarifs/Tarifs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 const Tarifs = () => {
@@ -7,35 +6,35 @@ const Tarifs = () => {
     <div className='all-container'>
       <h2 className='title-tarifs'>Tarifs</h2>
       <div className='tarifs-container'>
-        <span className='tarifs portrait'>
+        <Link className="tarif-link portrait" to="/Tarifs">
           <div className='photo photo-portrait' />
-          <Link className="btn-tarifs" to="/Tarifs">Juste moi</Link>
-        </span>
+          Juste moi
+        </Link>
 
-        <span className='tarifs pour-deux'>
-        <div className='photo photo-duo' />
-          <Link className="btn-tarifs" to="/Tarifs">Pour deux</Link>
-        </span>
+        <Link className="tarif-link pour-deux" to="/Tarifs">
+          <div className='photo photo-duo' />
+          Pour deux
+        </Link>
 
-        <span className='tarifs famille'>
-        <div className='photo photo-famille' />
-          <Link className="btn-tarifs" to="/Tarifs">Famille</Link>
-        </span>
+        <Link className="tarif-link famille" to="/Tarifs">
+          <div className='photo photo-famille' />
+          Famille
+        </Link>
 
-        <span className='tarifs grossesse'>
-        <div className='photo photo-grossesse' />
-          <Link className="btn-tarifs" to="/Tarifs">Il était une fois</Link>
-        </span>
+        <Link className="tarif-link grossesse" to="/Tarifs">
+          <div className='photo photo-grossesse' />
+          Il était une fois
+        </Link>
 
-        <span className='tarifs bebe'>
-        <div className='photo photo-bebe' />
-          <Link className="btn-tarifs" to="/Tarifs">Bébé</Link>
-        </span>
+        <Link className="tarif-link bebe" to="/Tarifs">
+          <div className='photo photo-bebe' />
+          Bébé
+        </Link>
 
-        <span className='tarifs evenement'>
-        <div className='photo photo-evenement' />
-          <Link className="btn-tarifs" to="/Tarifs">Evénement</Link>
-        </span>
+        <Link className="tarif-link evenement" to="/Tarifs">
+          <div className='photo photo-evenement' />
+          Evénement
+        </Link>
       </div>
     </div>
   );
