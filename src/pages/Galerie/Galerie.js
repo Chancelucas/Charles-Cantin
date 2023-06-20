e9dfe5bb6c58e112f5ae6740f5ded53bc43d082b
Chancelucas <105455769+Chancelucas@users.noreply.github.com>
2023-06-20 12:10:01 +0200

    Test de créeation fichier.json pour la galerie photo

--- src/pages/Galerie/Galerie.js
+++ src/pages/Galerie/Galerie.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from '../../components/Navigation/Navigation';
 import Footer from '../../components/Footer/Footer';
+import Photo from './Photo/Photo';
 
 const Galerie = () => {
   return (
     <div>
       <Navigation/>
+      <div>
+        <Photo/>
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/pages/Galerie/Photo/Photo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Photo = () => {
+  
+  return (
+    <div className="photo-container">
+      <div className="photo-bapteme">
+        <div className="photo bapteme-1"></div>
+        <span className="photo bapteme-2"></span>
+        <span className="photo bapteme-3"></span>
+        <span className="photo bapteme-4"></span>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Photo;
